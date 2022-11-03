fe50e4d904d41221e1ae409208f6e9c8427d42b7
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2022-11-03 16:35:30 +0800

    Исправил логику калькулятора

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
 
   let homePrice = $('#wpforms-896-field_1').val()
 
-  let carniz = homePrice * 0.17     // Подшив карнизов с водостоком
-  let okosycka = homePrice * 0.15   // Окосячка  (подготовка под монтаж окон и дверей)
-  let okna = homePrice * 0.1        // Окна
-  let dveri = homePrice * 0.13      // Двери
-  let uteplenie = homePrice * 0.09  // Утепление
-  let pol = homePrice * 0.12        // Пол чистовой
-  let potolok = homePrice * 0.11    // Потолок чистовой
+  let carniz = 0        // Подшив карнизов с водостоком
+  let okosycka = 0    // Окосячка  (подготовка под монтаж окон и дверей)
+  let okna = 0           // Окна
+  let dveri = 0        // Двери
+  let uteplenie = 0 // Утепление
+  let pol = 0             // Пол чистовой
+  let potolok = 0    // Потолок чистовой
 
   let brus = $('input[name="material"]:checked').val()
   let fundament = $('input[name="fundament"]:checked').val()
@@ -21,34 +21,59 @@ $(document).ready(function () {
   // ====================== Типо для вывод цен ===============================
 
   function CalcPrice() {
-    $('#materials .project-calculator__element:first-child .calculator-element__price').html(String(Math.round(homePrice * 0.125)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('#materials .project-calculator__element:nth-child(2) .calculator-element__price').html(String(Math.round(homePrice * 0.15)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('#materials .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.29)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('#materials .project-calculator__element:nth-child(4) .calculator-element__price').html(String(Math.round(homePrice * 0.3)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('#materials .project-calculator__element:nth-child(5) .calculator-element__price').html(String(Math.round(homePrice * 0.5)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#materials .project-calculator__element:nth-child(2) .calculator-element__price').html(String(Math.round(homePrice * 0.125)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#materials .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.15)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#materials .project-calculator__element:nth-child(4) .calculator-element__price').html(String(Math.round(homePrice * 0.29)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#materials .project-calculator__element:nth-child(5) .calculator-element__price').html(String(Math.round(homePrice * 0.3)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#materials .project-calculator__element:nth-child(6) .calculator-element__price').html(String(Math.round(homePrice * 0.5)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
     $('input#material_1').val(Math.round(homePrice * 0.125))
-    $('input#material_2').val(Math.round(homePrice * 0.15))
+    $('input#material_3').val(Math.round(homePrice * 0.15))
     $('input#material_4').val(Math.round(homePrice * 0.29))
     $('input#material_5').val(Math.round(homePrice * 0.3))
     $('input#material_6').val(Math.round(homePrice * 0.5))
 
     $('#fundaments .project-calculator__element:nth-child(2) .calculator-element__price').html(String(Math.round(homePrice * 0.2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('#fundaments .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.39)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('input#fundament_1').val(Math.round(homePrice * 0.2))
-    $('input#fundament_2').val(Math.round(homePrice * 0.39))
+    $('#fundaments .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.38)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('input#fundament_2').val(Math.round(homePrice * 0.2))
+    $('input#fundament_3').val(Math.round(homePrice * 0.39))
 
     $('#crovlys .project-calculator__element:nth-child(2) .calculator-element__price').html(String(Math.round(homePrice * 0.17)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
     $('#crovlys .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.19)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
     $('#crovlys .project-calculator__element:nth-child(4) .calculator-element__price').html(String(Math.round(homePrice * 0.23)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
     $('#crovlys .project-calculator__element:nth-child(5) .calculator-element__price').html(String(Math.round(homePrice * 0.3)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
-    $('input#crovly_1').val(Math.round(homePrice * 0.17))
-    $('input#crovly_2').val(Math.round(homePrice * 0.19))
-    $('input#crovly_3').val(Math.round(homePrice * 0.23))
-    $('input#crovly_4').val(Math.round(homePrice * 0.3))
+    $('input#crovly_2').val(Math.round(homePrice * 0.17))
+    $('input#crovly_3').val(Math.round(homePrice * 0.19))
+    $('input#crovly_4').val(Math.round(homePrice * 0.23))
+    $('input#crovly_5').val(Math.round(homePrice * 0.3))
+
+    $('#dops .project-calculator__element:nth-child(1) .calculator-element__price').html(String(Math.round(homePrice * 0.17)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(2) .calculator-element__price').html(String(Math.round(homePrice * 0.15)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(3) .calculator-element__price').html(String(Math.round(homePrice * 0.1)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(4) .calculator-element__price').html(String(Math.round(homePrice * 0.13)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(5) .calculator-element__price').html(String(Math.round(homePrice * 0.09)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(6) .calculator-element__price').html(String(Math.round(homePrice * 0.12)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+    $('#dops .project-calculator__element:nth-child(7) .calculator-element__price').html(String(Math.round(homePrice * 0.11)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' ₽')
+
+    $('input#carniz').val(Math.round(homePrice * 0.17))
+    $('input#okosycka').val(Math.round(homePrice * 0.15))
+    $('input#okna').val(Math.round(homePrice * 0.1))
+    $('input#dveri').val(Math.round(homePrice * 0.13))
+    $('input#uteplenie').val(Math.round(homePrice * 0.09))
+    $('input#pol').val(Math.round(homePrice * 0.12))
+    $('input#potolok').val(Math.round(homePrice * 0.11))
 
     brus = $('input[name="material"]:checked').val()
     fundament = $('input[name="fundament"]:checked').val()
     crovly = $('input[name="crovly"]:checked').val()
+    // carniz = $('input[name="carniz"]:checked').val()        // Подшив карнизов с водостоком
+    // okosycka = $('input[name="okosycka"]:checked').val()    // Окосячка  (подготовка под монтаж окон и дверей)
+    // okna = $('input[name="okna"]:checked').val()           // Окна
+    // dveri = $('input[name="dveri"]:checked').val()         // Двери
+    // uteplenie = $('input[name="uteplenie"]:checked').val() // Утепление
+    // pol = $('input[name="pol"]:checked').val()             // Пол чистовой
+    // potolok = $('input[name="potolok"]:checked').val()     // Потолок чистовой
+    // console.log(okosycka)
+
 
     calculateBasic()
   }
@@ -61,13 +86,6 @@ $(document).ready(function () {
 
   function startTest() {
     homePrice = $('#wpforms-896-field_1').val()
-    carniz = homePrice * 0.17     // Подшив карнизов с водостоком
-    okosycka = homePrice * 0.15   // Окосячка  (подготовка под монтаж окон и дверей)
-    okna = homePrice * 0.1        // Окна
-    dveri = homePrice * 0.13      // Двери
-    uteplenie = homePrice * 0.09  // Утепление
-    pol = homePrice * 0.12        // Пол чистовой
-    potolok = homePrice * 0.11
     CalcPrice()
   }
 
@@ -88,11 +106,69 @@ $(document).ready(function () {
     crovly = $(this).val()
     calculateBasic()
   })
+  $('input[name="okosycka"').change(function () {
+    if ($(this).is(':checked')) {
+      okosycka = $(this).val()
+    } else {
+      okosycka = 0
+    }
+    calculateBasic()
+  })
+  $('input[name="carniz"').change(function () {
+    if ($(this).is(':checked')) {
+      carniz = $(this).val()
+    } else {
+      carniz = 0
+    }
+    calculateBasic()
+  })
+  $('input[name="okna"').change(function () {
+    if ($(this).is(':checked')) {
+      okna = $(this).val()
+    } else {
+      okna = 0
+    }
+    calculateBasic()
+  })
+
+  $('input[name="dveri"').change(function () {
+    if ($(this).is(':checked')) {
+      dveri = $(this).val()
+    } else {
+      dveri = 0
+    }
+    calculateBasic()
+  })
+  $('input[name="uteplenie"').change(function () {
+    if ($(this).is(':checked')) {
+      uteplenie = $(this).val()
+    } else {
+      uteplenie = 0
+    }
+    calculateBasic()
+  })
+  $('input[name="pol"').change(function () {
+    if ($(this).is(':checked')) {
+      pol = $(this).val()
+    } else {
+      pol = 0
+    }
+    calculateBasic()
+  })
+  $('input[name="potolok"').change(function () {
+    if ($(this).is(':checked')) {
+      potolok = $(this).val()
+    } else {
+      potolok = 0
+    }
+    calculateBasic()
+  })
 
   function calculateBasic() {
-    let summ = Number(carniz) + Number(okosycka) + Number(okna) + Number(dveri) + Number(uteplenie) + Number(pol) + Number(potolok) + Number(brus) + Number(fundament) + Number(crovly)
+    let summ = Number(brus) + Number(fundament) + Number(crovly) + Number(carniz) + Number(okosycka) + Number(okna) + Number(dveri) + Number(uteplenie) + Number(pol) + Number(potolok)
+    let CurrentPrice = Number(homePrice) + Number(summ)
 
-    summ = String(Math.round(summ)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")
-    $('.calculator-tablo__title span').html(summ + ' ₽')
+    CurrentPrice = String(Math.round(CurrentPrice)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")
+    $('.calculator-tablo__title span').html(CurrentPrice + ' ₽')
   }
 })
